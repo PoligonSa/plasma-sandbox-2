b82906f9d14227d308eec34bf3fcf8a391c8ab89
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-09 10:07:10 +0700

    feat: add web

---
+++ packages/web/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.apiCall = void 0;
+exports.apiCall = function () {
+    console.log('apiCall');
+};
+//# sourceMappingURL=index.js.map
---
+++ packages/web/readme.md
@@ -0,0 +1,1 @@
+### Plasmax WEB
